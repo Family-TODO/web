7811f8c7fdadb003a7edc1306f48b0d8a032548c
Alexey Khruhsch <alexeykhr@outlook.com>
2019-01-07 15:17:13 +0200

    Add basic BaseButton

---
+++ src/components/base/button/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="base-button">
+		<button
+				v-bind="$attrs"
+				v-on="$listeners">
+			Button
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BaseButton',
+	inheritAttrs: false
+}
+</script>
