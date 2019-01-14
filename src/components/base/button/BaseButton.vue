29c4b2dc1b53dcf9c7b66510fe03e9afe60b5af4
Alexey Khruhsch <alexeykhr@outlook.com>
2019-01-14 23:02:48 +0200

    Update BaseButton (add icon, round props) + styles

--- src/components/base/button/BaseButton.vue
+++ src/components/base/button/BaseButton.vue
@@ -1,17 +1,29 @@
 <template>
-	<div class="base-button">
-		<button
-			v-bind="$attrs"
-			v-on="$listeners"
-		>
-			Button
-		</button>
-	</div>
+	<button
+		:class="['base-button', {
+			'base-button_icon': icon,
+			'base-button_round': round
+		}]"
+		v-bind="$attrs"
+		v-on="$listeners"
+	>
+		<slot />
+	</button>
 </template>
 
 <script>
 export default {
 	name: 'BaseButton',
-	inheritAttrs: false
+	inheritAttrs: false,
+	props: {
+		icon: {
+			type: Boolean,
+			default: false
+		},
+		round: {
+			type: Boolean,
+			default: false
+		}
+	}
 }
 </script>
