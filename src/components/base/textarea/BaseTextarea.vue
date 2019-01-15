798cae68fdd4b6e1128fa1be239f5ed8a9cc1c05
Alexey Khruhsch <alexeykhr@outlook.com>
2019-01-15 08:51:31 +0200

    Add BaseTextarea

---
+++ src/components/base/textarea/BaseTextarea.vue
@@ -0,0 +1,31 @@
+<template>
+	<textarea
+		:value="value"
+		class="base-textarea"
+		rows="5"
+		v-bind="$attrs"
+		v-on="listeners"
+	/>
+</template>
+
+<script>
+export default {
+	name: 'BaseTextarea',
+	props: {
+		value: {
+			type: String,
+			default: ''
+		}
+	},
+	computed: {
+		listeners() {
+			return {
+				...this.$listeners,
+				input: (evt) => {
+					this.$emit('input', evt.target.value)
+				}
+			}
+		}
+	}
+}
+</script>
---
+++ src/components/base/textarea/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+import Component from './BaseTextarea'
+
+export default (Vue) => Vue.component(Component.name, Component)
